d0806659b006bd7140277c7c4a68e09898677a25
Daryl Sew <darylsew@gmail.com>
2014-07-06 18:29:16 -0700

    Reorganize files.. things starting to come together

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def home():
     #params = request.getParams()
     #lat = param['lat']
     #lon = param['lon']
@@ -11,5 +11,13 @@ def hello_world():
     #database.add(lat, lon, amp, freq)
     return render_template("index.html")
 
+@app.route("/recorder")
+def audio():
+    return render_template("recorder.html")
+
+@app.route("/map")
+def map():
+    return render_template("map.html")
+
 if __name__ == '__main__':
-    app.run()
+    app.run(port=9999)
