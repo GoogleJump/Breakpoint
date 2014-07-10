468aa3f827e3b478cc706fb5426d5c9fc33e1988
Daryl Sew <darylsew@gmail.com>
2014-07-09 23:58:48 -0700

    More progress on login server code

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from functools import wraps
-from flask import Flask, render_template, redirect, url_for, g, request
+from flask import Flask, render_template, redirect, url_for, g, request, session
                     
 app = Flask(__name__)
+app.secret_key = 'secret_key'
 
 """
 Supporting stuff.
@@ -30,34 +31,49 @@ def home():
     #database.add(lat, lon, amp, freq)
     login_manager = LoginManager()
     login_manager.init_app(app)
-    return render_template("index.html")
+    return render_template('index.html')
 
-@app.route("/recorder")
+@app.route('/recorder')
 def audio():
-    return render_template("recorder.html")
+    return render_template('recorder.html')
 
-@app.route("/map")
+@app.route('/map')
 def map():
-    return render_template("map.html")
+    return render_template('map.html')
 
 
-@app.route("/login", methods=["GET", "POST"])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        flash('logged in!')
+        return redirect(url_for('index'))
+    return """
+        <form action="" method="post">
+            <p><input type=text name=username>
+            <p><input type=submit value=Login>
+        </form>
+    """
+
     form = LoginForm()
     if form.validate_on_submit():
         # login and validate the user...
         login_user(user)
-        flash("Logged in successfully.")
+        flash('Logged in successfully.')
         # TODO where to redirect to?
-        return redirect(url_for("index"))
-    return render_template("login.html", form=form)
+        return redirect(url_for('index'))
+    return render_template('login.html', form=form)
 
-@app.route("/logout")
+@app.route('/logout')
 @login_required
 def logout():
-    logout_user()
-    return redirect(url_for("index"))
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
-    app.run(port=9999)
+    app.port = 9999
+    # FIXME: Disable debug mode in prodoction!
+    app.debug = True
+    app.run()
